75f9635d0118ef072eb5a10f7930e9d3f908a073
Pauline Barnades <pauline.barnades@stenaline.com>
2023-07-09 18:27:03 +0200

    clean up the code

--- contract/balanceOf.sol
+++ contract/balanceOf.sol
@@ -3,21 +3,14 @@ pragma solidity ^0.8.0;
 import { ISuperToken } from "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/ISuperToken.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract StreamChecker {
-    ISuperToken fUSDCx; 
-    address constant vitalikEthAddress = 0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045; 
+contract superfluid {
+    
+     address private constant fUSDCxAddress = 0xbe49ac1EadAc65dccf204D4Df81d650B50122aB2; 
+   address private constant vitalikAddress = address(0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045);
+    // Address of the fUSDCx token contract
 
-    constructor() {
-        fUSDCx = ISuperToken(0xbe49ac1EadAc65dccf204D4Df81d650B50122aB2);
-    }
-
-    function balanceOf(address account) external view returns(bool) {
-        (int96 flowRate,,,) = fUSDCx.getFlow(account, vitalikEthAddress);
-        if (flowRate > 0) {
-            return true;
-        } else {
-            return false;
-        }
+    function hasActiveStreamToVitalik(address) external view returns (uint256) {
+        ISuperToken fUSDCx = ISuperToken(fUSDCxAddress);
+        return fUSDCx.balanceOf(vitalikAddress) > 0 ? 1 : 0;
     }
 }
-
